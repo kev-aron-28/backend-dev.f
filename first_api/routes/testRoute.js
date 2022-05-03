db15388ebd206410065474a9b8f1f13f16f0440b
Kevaron <kevtapia75@gmail.com>
2022-05-02 21:01:13 -0500

    excercise 1

--- first_api/routes/testRoute.js
+++ first_api/routes/testRoute.js
@@ -1,9 +1,16 @@
 const { Router } = require("express")
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        msg: 'Hola mundo'
+router.get('/string/:s', (req, res) => {
+    const { s } = req.params
+    if(s.length > 10) {
+        return res.status(500).json({ 
+            msg: 'Error'
+        })
+    }
+
+    return res.json({
+        string: s.toUpperCase()
     })
 })
 
